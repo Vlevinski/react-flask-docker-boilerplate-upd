7ccac00f590a9cbb60cf8c2667b5937de193883d
Vlevinski <v.levinski@gmail.com>
2022-07-10 18:18:06 +0300

    Minor updates, test

--- api/app.py
+++ api/app.py
@@ -2,19 +2,26 @@ from flask import Flask, jsonify, make_response
 
 app = Flask(__name__)
 
-category, price, stocked, name, true, false = "category", "price", "stocked", "name", 1, 0
+false, true = False, True
+category, price, stocked, name = "category", "price", "stocked", "name"
+
+
+@app.route('/')
+def home_response():
+    return "/api/v1.0/test"
+
 
 @app.route('/api/v1.0/test', methods=['GET'])
 def test_response():
     """Return a sample JSON response."""
     sample_response = {
         "items": [
-  {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
-  {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
-  {category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
-  {category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
-  {category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
-  {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
+            {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
+            {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
+            {category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
+            {category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
+            {category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
+            {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
         ]
     }
     # JSONify response
